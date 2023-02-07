3784f2bdf722acf7ff818d4d412718008a5ade25
ijang <ijang@175.115.183.76>
2023-02-07 17:29:28 +0900

    @Configuration 추가

--- src/main/java/houzz/DataBaseInfo.java
+++ src/main/java/houzz/DataBaseInfo.java
@@ -7,8 +7,9 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class DataBaseInfo {
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource, 

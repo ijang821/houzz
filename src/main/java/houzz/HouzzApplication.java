fdfb667d16630d313a74ce7e1235dd41615ff3b8
ijang <ijang@175.115.183.76>
2023-02-07 12:34:45 +0900

    houzz 시작

---
+++ src/main/java/houzz/HouzzApplication.java
@@ -0,0 +1,26 @@
+package houzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@SpringBootApplication
+@Controller
+public class HouzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouzzApplication.class, args);
+	}
+	
+	
+	@RequestMapping("/")
+	public String index(Model model,HttpServletRequest request) {
+
+		return "thymeleaf/index";
+	}
+
+}
---
+++ src/test/java/houzz/HouzzApplicationTests.java
@@ -0,0 +1,13 @@
+package houzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

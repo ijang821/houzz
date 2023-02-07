72808404a3b85896b0142a8e471876a3ce2a84c5
ijang <ijang@175.115.183.76>
2023-02-07 15:51:30 +0900

    member regist method=get

--- src/main/java/houzz/command/MemberCommand.java
+++ src/main/java/houzz/command/MemberCommand.java
@@ -1,8 +1,20 @@
 package houzz.command;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class MemberCommand {
-	
+	String memberNum;
+	String memberName;
+	String memberId;
+	String memberPw;
+	String memberPwCon;
+	String memberPhone;
+	String memberEmail;
+	String memberAddr;
+	String memberGender;
+	Date memberBirth;
+	String accountAddress;
 }
--- src/main/java/houzz/controller/MemberController.java
+++ src/main/java/houzz/controller/MemberController.java
@@ -7,10 +7,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("member")
 public class MemberController {
+	/**
+	 * 회원 초기 페이지
+	 * @return
+	 */
 	@RequestMapping("memberList")
 	public String memberList() {
 		return "thymeleaf/member/memberList";
 	}
+	
+	/**
+	 * 회원 등록
+	 * @return
+	 */
 	@RequestMapping(value = "memberRegist", method = RequestMethod.GET)
 	public String memberRegist() {
 		return "thmyeleaf/member/memberForm";

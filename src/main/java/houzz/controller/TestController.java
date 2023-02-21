284589bd9d6c3c96821ed65fe76fe35d5ba93c9f
hk_edu <hk_edu@DESKTOP-K8UD7GT>
2023-02-21 17:32:03 +0900

    사업자등록번호 조회 test

---
+++ src/main/java/houzz/controller/TestController.java
@@ -0,0 +1,12 @@
+package houzz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	@GetMapping("/test")
+	public String test() {
+		return "thymeleaf/mediation/test";
+	}
+}
